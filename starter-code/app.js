a42bb3aae18b769fef16d21d3a043ac7e30c266e
Peter Luna <lunapeter92@gmail.com>
2021-06-11 16:51:26 -0400

    finished problem 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -32,7 +32,7 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
   let multiple = a*b;
   let string = "The product of " + a + " and " + b + " is " + multiple + ".";
-  return [multiple, string];
+  return [multiple, string]
 }
 
 // Here is the test for multiply(); uncomment it to run it
